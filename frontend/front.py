e17039224198f285a70e51f45e3f18d6bc4eded6
Roger Mauvois <roger.mauvois@gmail.com>
2024-06-19 12:18:18 +0200

    front.py refactor

--- frontend/front.py
+++ frontend/front.py
@@ -7,28 +7,30 @@ import re
 from styles import external_stylesheets, index_string
 
 # Backend API URL (Update this URL after deploying the backend)
-BACKEND_API_URL = "https://backend-5ols2im5la-ew.a.run.app"
-INTERPRETATION_API_URL = "https://interpretation-5ols2im5la-ew.a.run.app"
+BACKEND_API_URL = os.environ.get(
+    "BACKEND_API_URL", "https://backend-5ols2im5la-ew.a.run.app")
+INTERPRETATION_API_URL = os.environ.get(
+    "INTERPRETATION_API_URL", "https://interpretation-5ols2im5la-ew.a.run.app")
 
 # Initialize the Dash app with a theme and custom font
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 app.index_string = index_string
 
-app.layout = html.Div([
+# Commonly used styles
+HIDDEN = {'display': 'none'}
+VISIBLE = {'display': 'block'}
+BUTTON_STYLE = {'display': 'inline-block'}
 
-    # Part 1: Ask Question
+app.layout = html.Div([
     html.Div(id='part-1', children=[
         html.Div('Quelle est votre question pour l\'Oracle?',
                  className='question-label'),
         dcc.Input(id='question-input', type='text', debounce=True,
-                  placeholder='', className='form-control my-3', value=''),  # Added value=''
-        html.Button('', id='submit-question-btn',
-                    n_clicks=0, className='btn btn-transparent my-3')
+                  placeholder='', className='form-control my-3', value=''),
+        html.Button('', id='submit-question-btn', n_clicks=0,
+                    className='btn btn-transparent my-3')
     ], className="container"),
 
-
-    # Part 2: Initialize Toss and Generate Lines (Initially Hidden)
     html.Div(id='part-2', children=[
         html.Div(id='question-display', className='question-display my-3'),
         html.H3("Generate 6 Lines", id='generate-6-lines-title',
@@ -46,9 +48,8 @@ app.layout = html.Div([
         ], className='my-3'),
         html.Div(id='line-output', className='alert alert-info line-recap'),
         html.Div(id='line-type-output', className='line-type')
-    ], className="container", style={'display': 'none'}),
+    ], className="container", style=HIDDEN),
 
-    # Part 3: Display Hexagram and Interpretation (Initially Hidden)
     html.Div(id='part-3', children=[
         html.Div(id='question-display-3', className='question-display my-3'),
         html.Div(id='hexagram-output', className="container my-5"),
@@ -56,12 +57,8 @@ app.layout = html.Div([
         html.Button('Get Interpretation', id='get-interpretation-btn',
                     n_clicks=0, className='btn btn-info my-3'),
         html.Div(id='interpretation-output', className='alert alert-warning')
-    ], className="container", style={'display': 'none'})
-
+    ], className="container", style=HIDDEN)
 ])
-
-# Define the base URL of your API
-BASE_URL = BACKEND_API_URL
 
 # Define global variables
 start_time = None
@@ -70,6 +67,14 @@ lines = []
 random_state = None
 
 # Helper functions
+
+
+def api_request(url, json_data):
+    response = requests.post(url, json=json_data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.json().get('detail')}
 
 
 def start_timer():
@@ -89,51 +94,38 @@ def stop_timer(index):
         return elapsed_time
 
 
-def process_line(random_state):
-    global stop_times, lines
-    response = requests.post(
-        f"{BASE_URL}/generate-line",
-        json={"times": stop_times, "random_state": random_state}
-    )
-    if response.status_code == 200:
-        result = response.json()
+def process_line():
+    global stop_times, lines, random_state
+    result = api_request(f"{BACKEND_API_URL}/generate-line",
+                         {"times": stop_times, "random_state": random_state})
+    if "error" not in result:
         lines.append(result['line_sum'])
         return f"Line Type: {result['line_type']}, Line Sum: {result['line_sum']}"
-    else:
-        return f"Error: {response.json().get('detail')}"
+    return f"Error: {result['error']}"
 
 
 def get_hexagram():
-    response = requests.post(
-        f"{BASE_URL}/get-hexagram", json={"line_values": lines}
-    )
-    if response.status_code == 200:
-        hexagram = response.json().get("hexagram")
-        return hexagram
-    else:
-        return None
+    result = api_request(
+        f"{BACKEND_API_URL}/get-hexagram", {"line_values": lines})
+    if "error" not in result:
+        return result['hexagram']
+    return None
 
 
 def render_hexagram_line(line_value):
-    if (line_value % 2) == 0:  # Yin line (broken)
-        return html.Div(className='hexagram-line broken')
-    else:  # Yang line (solid)
-        return html.Div(className='hexagram-line solid')
+    return html.Div(className='hexagram-line solid' if line_value % 2 else 'hexagram-line broken')
 
 
 def get_interpretation(question, iching_response):
-    response = requests.post(
-        f"{INTERPRETATION_API_URL}/interpret",
-        json={"question": question, "iching_response": iching_response}
-    )
-    if response.status_code == 200:
-        interpretation = response.json().get("interpretation")
-        return interpretation
-    else:
-        return f"Error: {response.json().get('detail')}"
-
+    result = api_request(f"{INTERPRETATION_API_URL}/interpret",
+                         {"question": question, "iching_response": iching_response})
+    if "error" not in result:
+        return result['interpretation']
+    return f"Error: {result['error']}"
 
 # Callbacks
+
+
 @app.callback(
     Output('part-1', 'style'),
     Output('part-2', 'style'),
@@ -149,15 +141,14 @@ def submit_question(n_clicks, n_submit, question):
     n_clicks = n_clicks or 0
     n_submit = n_submit or 0
     if (n_clicks > 0 or n_submit > 0) and question:
-        response = requests.post(
-            f"{BASE_URL}/initialize-toss", json={"text": question})
-        if response.status_code == 200:
-            random_state = response.json().get("random_state")
+        result = api_request(
+            f"{BACKEND_API_URL}/initialize-toss", {"text": question})
+        if "error" not in result:
+            random_state = result.get("random_state")
             lines.clear()
-            return {'display': 'none'}, {'display': 'block'}, {'display': 'none'}, f"{question}", f"Question: {question}"
-        else:
-            return {'display': 'block'}, {'display': 'none'}, {'display': 'none'}, f"Error: {response.json().get('detail')}", ""
-    return {'display': 'block'}, {'display': 'none'}, {'display': 'none'}, "", ""
+            return HIDDEN, VISIBLE, HIDDEN, f"{question}", f"Question: {question}"
+        return VISIBLE, HIDDEN, HIDDEN, f"Error: {result['error']}", ""
+    return VISIBLE, HIDDEN, HIDDEN, "", ""
 
 
 @app.callback(
@@ -182,34 +173,31 @@ def manage_timers(start_clicks, stop1_clicks, stop2_clicks, stop3_clicks):
 
     if 'stop-timer' in button_id:
         try:
-            match = re.search(r'stop-timer(\d+)-btn', button_id)
-            if match:
-                index = int(match.group(1)) - 1
-                elapsed_time = stop_timer(index)
-                if elapsed_time is not None:
-                    if len(stop_times) == 3:
-                        result = process_line(random_state)
-                        stop_times.clear()
-                        if len(lines) == 6:
-                            hexagram = get_hexagram()
-                            hexagram_lines = [render_hexagram_line(
-                                line) for line in lines[::-1]]
-                            hexagram_details = [
-                                html.Br(),
-                                html.Div(className='hexagram-title',
-                                         children=f"Hexagramme {hexagram[0]} — {hexagram[1]} "),
-                                html.Br(),
-                                html.Div(className='hexagram-section',
-                                         children=f"{hexagram[4]}"),
-                                *[html.Div(className='hexagram-section', children=f"{detail}") for detail in hexagram[5:]]
-                            ]
-                            hexagram_output = html.Div(
-                                children=hexagram_lines + hexagram_details, className='container hexagram-details')
-                            return result, hexagram_output
-                        return result, ""
-                    return f"Timer stopped {index + 1} times: {elapsed_time} ms", ""
-            else:
-                return "Error processing timer: Invalid button ID format", ""
+            index = int(re.search(r'stop-timer(\d+)-btn',
+                        button_id).group(1)) - 1
+            elapsed_time = stop_timer(index)
+            if elapsed_time is not None:
+                if len(stop_times) == 3:
+                    result = process_line()
+                    stop_times.clear()
+                    if len(lines) == 6:
+                        hexagram = get_hexagram()
+                        hexagram_lines = [render_hexagram_line(
+                            line) for line in lines[::-1]]
+                        hexagram_details = [
+                            html.Br(),
+                            html.Div(
+                                className='hexagram-title', children=f"Hexagramme {hexagram[0]} — {hexagram[1]} "),
+                            html.Br(),
+                            html.Div(className='hexagram-section',
+                                     children=f"{hexagram[4]}"),
+                            *[html.Div(className='hexagram-section', children=f"{detail}") for detail in hexagram[5:]]
+                        ]
+                        hexagram_output = html.Div(
+                            children=hexagram_lines + hexagram_details, className='container hexagram-details')
+                        return result, hexagram_output
+                    return result, ""
+                return f"Timer stopped {index + 1} times: {elapsed_time} ms", ""
         except (IndexError, ValueError) as e:
             return f"Error processing timer: {str(e)}", ""
 
@@ -239,7 +227,6 @@ def update_display(timer_output):
         line_recap = html.Div(children=[html.Div(
             f"{i+1}: {line}") for i, line in enumerate(lines)], className='my-3')
 
-    hexagram_details = []
     if len(lines) == 6:
         hexagram = get_hexagram()
         if hexagram:
@@ -258,13 +245,13 @@ def update_display(timer_output):
             hexagram_output = html.Div(
                 "Error fetching hexagram details", className='alert alert-danger')
 
-        button_style = {'display': 'none'}
-        title_style = {'display': 'none'}
-        line_type_style = {'display': 'none'}
+        button_style = HIDDEN
+        title_style = HIDDEN
+        line_type_style = HIDDEN
     else:
-        button_style = {'display': 'inline-block'}
-        title_style = {'display': 'block'}
-        line_type_style = {'display': 'block'}
+        button_style = BUTTON_STYLE
+        title_style = VISIBLE
+        line_type_style = VISIBLE
 
     return line_recap, hexagram_output, button_style, button_style, button_style, button_style, title_style, line_type_style
 
